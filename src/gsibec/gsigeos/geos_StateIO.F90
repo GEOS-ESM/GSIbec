592cd6fb5479498537746a75dac553817b3a03c1
Ricardo Todling <ricardo.todling@nasa.gov>
2023-03-15 15:59:40 -0400

    initial attempt to call hyb

--- src/gsibec/gsigeos/geos_StateIO.F90
+++ src/gsibec/gsigeos/geos_StateIO.F90
@@ -1,5 +1,9 @@
 module geos_StateIO
 use m_kinds, only: i_kind
+use m_mpimod, only: mype,npe
+
+use m_read_geosens, only: geos_readens
+
 implicit none
 private
 public :: state_get
@@ -12,6 +16,8 @@ end interface
 interface state_put
    module procedure state_put_ 
 end interface
+
+character(len=*), parameter :: myname = 'geos_StateIO'
 contains
 subroutine get_1state_(xx,sgrid,nymd,nhms,iwhat,tau)
 use gsi_bundlemod, only: gsi_bundle
@@ -23,16 +29,32 @@ type(sub2grid_info),intent(in   ) :: sgrid ! internal subdomain grid
 integer(i_kind),    intent(in   ) :: nymd,nhms
 integer(i_kind),    intent(in   ) :: iwhat
 integer(i_kind),optional,intent(in ) :: tau   ! time interval in hours
+character(len=*), parameter :: myname_ = myname//'get_1state_'
+print *, 'DEBUG: get_1state_ - getting here '
 end subroutine get_1state_
 subroutine get_Nstate_(xx,sgrid,nymd,nhms,tau)
+use constants, only: zero
 use gsi_bundlemod, only: gsi_bundle
 use gsi_bundlemod, only: assignment(=)
+use gsi_bundlemod, only: gsi_bundlegetpointer
 use general_sub2grid_mod, only: sub2grid_info
 implicit none
 type(gsi_bundle),   intent(inout) :: xx(:)
 type(sub2grid_info),intent(in   ) :: sgrid ! internal subdomain grid
 integer(i_kind),    intent(in   ) :: nymd,nhms
 integer(i_kind),optional,intent(in ) :: tau   ! time interval in hours
+
+character(len=*), parameter :: myname_ = myname//'get_Nstate_'
+integer ii,jj,istatus
+character(len=255),allocatable :: fnames(:)
+
+allocate(fnames(size(xx)))
+do ii=1,size(xx)
+   write(fnames(ii),'(a,i3.3,a)') 'mem', ii, '/f525_p7_fp.bkg.eta.20201215_00z.nc4' ! wired for now
+enddo
+call geos_readens(xx,fnames,npe,mype,0)
+deallocate(fnames)
+
 end subroutine get_Nstate_
 subroutine state_put_(xx,sgrid,nymd,nhms,member)
 use gsi_bundlemod, only: gsi_bundle
