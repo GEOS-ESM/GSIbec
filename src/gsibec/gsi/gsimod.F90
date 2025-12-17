8a1e0a302a9c4ed82b6e8b328414403676852b70
Masanori-NOAA <masanori.oigawa@noaa.gov>
2025-12-17 08:49:09 -0600

    Refactor: remove redundant code

--- src/gsibec/gsi/gsimod.F90
+++ src/gsibec/gsi/gsimod.F90
@@ -703,10 +703,6 @@
      if (mype==0) write(6,*)'GSIMOD:  tendencies and derivatives are on'
   endif
 
-  if (regional) then
-     call convert_fv3_regional
-  endif
-
 ! Initialize variables, create/initialize arrays
   lendian_in = -1
   call create_ges_tendencies(tendsflag,thisrc)
