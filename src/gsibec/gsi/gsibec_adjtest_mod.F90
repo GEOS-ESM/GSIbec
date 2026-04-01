b37063ce24e5d89fb79d11fc16abd2b4fbc2ed33
Ricardo Todling <ricardo.todling@nasa.gov>
2026-04-01 17:27:49 -0400

    Revise Internal AD Tests

--- src/gsibec/gsi/gsibec_adjtest_mod.F90
+++ src/gsibec/gsi/gsibec_adjtest_mod.F90
@@ -41,26 +41,33 @@ use compact_diffs, only: stvp2uv, tstvp2uv
 
 implicit none
 private
-public adtest_cv
-public adtest_bkgcov
+public adtest_cv1,adtest_cv2
+public adtest_bkgerr
 public adtest_stvp2uv
 public iadtest
+public iadtest_maxiter
+
+interface adtest_bkgerr
+  module procedure adtest_bkgerr_
+end interface adtest_bkgerr
 
 type(control_vector),save :: xtest1,xtest2
 
 ! iadtest           - adjoint tests:
-!                     (1) test CV 
-!                     (2) test B
-!                     (3) test stvp2uv
+!                     (1) test CV1 
+!                     (2) test CV2 - another flavor
+!                     (3) test B - full cov
+!                     (4) test stvp2uv
 
-logical, save :: iadtest(3)=.false.
+logical, save :: iadtest(4)=.false.
 
+integer :: iadtest_maxiter=0
 integer :: nrclen=0
-integer, save :: iadtest_cnt(3)=0
+integer, save :: iadtest_cnt(4)=0
 ! ----------------------------------------------------------------------
 contains
 ! ----------------------------------------------------------------------
-subroutine adtest_cv0(xhat)
+subroutine adtest_cv1(xhat)
 !$$$  subprogram documentation block
 !                .      .    .                                       .
 ! subprogram:    adtest
@@ -93,9 +100,9 @@ type(predictors) :: sbias1,sbias2
 integer(i_kind) :: ii,idig
 real(r_kind) :: zz1,zz2,zz3
 
-if (iadtest_cnt(1)>0) return
+if (iadtest_cnt(1)>iadtest_maxiter) return
 
-if (mype==0) write(6,*)'ADTEST_CV starting'
+if (mype==0) write(6,*)'ADTEST_CV1 starting'
 
 ! ----------------------------------------------------------------------
 ! Allocate local variables
@@ -111,10 +118,10 @@ call allocate_preds(sbias2)
 ! Initialize control space vectors
 if (present(xhat)) then
    xtest1=xhat
-   if (mype==0) write(6,*)'ADTEST_CV use input xhat'
+   if (mype==0) write(6,*)'ADTEST_CV1 use input xhat'
 else
    call random_cv(xtest1)
-   if (mype==0) write(6,*)'ADTEST_CV use random_cv(xhat)'
+   if (mype==0) write(6,*)'ADTEST_CV1 use random_cv(xhat)'
 endif
 xtest2=zero
 
@@ -150,15 +157,15 @@ if ( abs(zz1+zz2) > sqrt(tiny(zz3)) ) then
 else
    zz3=abs(zz1-zz2)
 endif
-idig= int(-log(zz3+tiny(zz3))/log(10.0_r_kind))
+idig= int(-log(abs(zz3)+tiny(zz3))/log(10.0_r_kind))
 
 if (mype==0) then
-   write(6,'(A)')' ADTEST_CV              0.123456789012345678'
-   write(6,'(A,ES25.18)')' ADTEST_CV <F''*F.Y,X>= ',zz1
-   write(6,'(A,ES25.18)')' ADTEST_CV < F.Y,F.X>= ',zz2
-   write(6,'(A,i3,   A)')' ADTEST_CV ',idig,' digits are identical'
-   write(6,'(A,ES25.18)')' ADTEST_CV rel. err.= ',zz3
-   write(6,'(A,ES25.18)')' ADTEST_CV mach.eps = ',epsilon(zz3)
+   write(6,'(A)')' ADTEST_CV1              0.123456789012345678'
+   write(6,'(A,ES25.18)')' ADTEST_CV1 <F''*F.Y,X>= ',zz1
+   write(6,'(A,ES25.18)')' ADTEST_CV1 < F.Y,F.X>= ',zz2
+   write(6,'(A,i3,   A)')' ADTEST_CV1 ',idig,' digits are identical'
+   write(6,'(A,ES25.18)')' ADTEST_CV1 rel. err.= ',zz3
+   write(6,'(A,ES25.18)')' ADTEST_CV1 mach.eps = ',epsilon(zz3)
 endif
 
 ! Release local variables
@@ -172,14 +179,14 @@ call deallocate_preds(sbias1)
 call deallocate_preds(sbias2)
 ! ----------------------------------------------------------------------
 
-if (mype==0) write(6,*)'ADTEST_CV finished'
+if (mype==0) write(6,*)'ADTEST_CV1 finished'
 
 iadtest_cnt(1) = iadtest_cnt(1) + 1
 
 return
-end subroutine adtest_cv0
+end subroutine adtest_cv1
 ! ----------------------------------------------------------------------
-subroutine adtest_cv(xhat)
+subroutine adtest_cv2(xhat)
 !$$$  subprogram documentation block
 !                .      .    .                                       .
 ! subprogram:    adtest
@@ -214,9 +221,9 @@ type(predictors) :: sbias1,sbias2
 integer(i_kind) :: ii,idig
 real(r_kind) :: zz1,zz2,zz3
 
-if (iadtest_cnt(1)>0) return
+if (iadtest_cnt(2)>iadtest_maxiter) return
 
-if (mype==0) write(6,*)'ADTEST_CV starting'
+if (mype==0) write(6,*)'ADTEST_CV2 starting'
 
 ! ----------------------------------------------------------------------
 ! Allocate local variables
@@ -232,10 +239,10 @@ call allocate_preds(sbias2)
 ! Initialize control space vectors
 if (present(xhat)) then
    xtest1=xhat
-   if (mype==0) write(6,*)'ADTEST_CV use input xhat'
+   if (mype==0) write(6,*)'ADTEST_CV2 use input xhat'
 else
    call random_cv(xtest1)
-   if (mype==0) write(6,*)'ADTEST_CV use random_cv(xhat)'
+   if (mype==0) write(6,*)'ADTEST_CV2 use random_cv(xhat)'
 endif
 call random_cv(xtest2,kseed=1999)
 
@@ -272,15 +279,15 @@ if ( abs(zz1+zz2) > sqrt(tiny(zz3)) ) then
 else
    zz3=abs(zz1-zz2)
 endif
-idig= int(-log(zz3+tiny(zz3))/log(10.0_r_kind))
+idig= int(-log(abs(zz3)+tiny(zz3))/log(10.0_r_kind))
 
 if (mype==0) then
-   write(6,'(A)')' ADTEST_CV                0.123456789012345678'
-   write(6,'(A,ES25.18)')' ADTEST_CV <F''*F.Y,  X>= ',zz1
-   write(6,'(A,ES25.18)')' ADTEST_CV <   F.Y,F.X>= ',zz2
-   write(6,'(A,i3,   A)')' ADTEST_CV ',idig,' digits are identical'
-   write(6,'(A,ES25.18)')' ADTEST_CV rel. err.= ',zz3
-   write(6,'(A,ES25.18)')' ADTEST_CV mach.eps = ',epsilon(zz3)
+   write(6,'(A)')' ADTEST_CV2                0.123456789012345678'
+   write(6,'(A,ES25.18)')' ADTEST_CV2 <F''*F.Y,  X>= ',zz1
+   write(6,'(A,ES25.18)')' ADTEST_CV2 <   F.Y,F.X>= ',zz2
+   write(6,'(A,i3,   A)')' ADTEST_CV2 ',idig,' digits are identical'
+   write(6,'(A,ES25.18)')' ADTEST_CV2 rel. err.= ',zz3
+   write(6,'(A,ES25.18)')' ADTEST_CV2 mach.eps = ',epsilon(zz3)
 endif
 
 ! Release local variables
@@ -294,63 +301,91 @@ call deallocate_preds(sbias1)
 call deallocate_preds(sbias2)
 ! ----------------------------------------------------------------------
 
-if (mype==0) write(6,*)'ADTEST_CV finished'
+if (mype==0) write(6,*)'ADTEST_CV2 finished'
 
-iadtest_cnt(1) = iadtest_cnt(1) + 1
+iadtest_cnt(2) = iadtest_cnt(2) + 1
 
 return
-end subroutine adtest_cv
+end subroutine adtest_cv2
 ! ----------------------------------------------------------------------
-subroutine adtest_bkgcov (s)
-use gsi_bundlemod, only: gsi_bundlecreate
-use gsi_bundlemod, only: assignment(=)
-use state_vectors, only: set_random
-use state_vectors, only: dot_product
-use state_vectors, only: prt_state_norms
+subroutine adtest_bkgerr_ (g1,g2)
+use control_vectors, only: control_vector
+use control_vectors, only: allocate_cv
+use control_vectors, only: deallocate_cv
+use control_vectors, only: allocate_cv,deallocate_cv
+use control_vectors, only: assignment(=)
+use control_vectors, only: random_cv
+use control_vectors, only: prt_control_norms
+use jfunc, only: nsclen,npclen,ntclen
+use hybrid_ensemble_parameters,only : l_hyb_ens
+use hybrid_ensemble_isotropic, only: bkerror_a_en
 implicit none
-type(gsi_bundle) :: s
-type(gsi_bundle) :: xs
-type(gsi_bundle) :: ys
-type(gsi_bundle) :: zs
+type(control_vector), optional :: g1,g2
+type(control_vector) :: gx
+type(control_vector) :: gy
+type(control_vector) :: gz
 integer(i_kind) :: idig, istatus
 real(r_kind) :: zz1,zz2,zz3
 
-if (iadtest_cnt(2) > 0) return
+if (iadtest_cnt(3)>2*iadtest_maxiter+1) return
 
-call gsi_bundlecreate ( xs, s, 'control2state work x', istatus )
-call gsi_bundlecreate ( ys, s, 'control2state work y', istatus )
-call gsi_bundlecreate ( zs, s, 'control2state work x', istatus )
-call set_random(xs)
-call set_random(ys,myseed=4567)
-call prt_state_norms(xs,'x')
-call prt_state_norms(ys,'y')
+call allocate_cv(gx)
+call allocate_cv(gy)
+call allocate_cv(gz)
 
-zs = ys
-call bkgcov(zs)
-zz1 = dot_product(zs,xs)
-zs = xs
-call bkgcov(zs)
-zz2 = dot_product(ys,zs)
+if (present(g1).and.present(g2) ) then
+  gx = g1
+  gy = g2
+else
+  call random_cv(gx,kseed=8765)
+  call random_cv(gy,kseed=54321)
+endif
+call prt_control_norms(gx,'ADTEST_BKGERR: gx')
+call prt_control_norms(gy,'ADTEST_BKGERR: gy')
+
+! z=By
+gz=zero
+call bkerror(gy,gz, &
+             1,nsclen,npclen,ntclen)
+if (l_hyb_ens) then
+   call bkerror_a_en(gy,gz)
+endif
+call prt_control_norms(gz,'ADTEST_BKGERR: By')
+zz1 = dot_product(gz,gx) ! (By)''*x
+
+! z=Bx
+gz=zero
+call bkerror(gx,gz, &
+             1,nsclen,npclen,ntclen)
+if (l_hyb_ens) then
+   call bkerror_a_en(gx,gz)
+endif
+call prt_control_norms(gz,'ADTEST_BKGERR: Bx')
+zz2 = dot_product(gy,gz) !  y''(Bx)
 
 if ( abs(zz1+zz2) > sqrt(tiny(zz3)) ) then
    zz3=two*abs(zz1-zz2)/(zz1+zz2)
 else
    zz3=abs(zz1-zz2)
 endif
-idig= int(-log(zz3+tiny(zz3))/log(10.0_r_kind))
+idig= int(-log(abs(zz3)+tiny(zz3))/log(10.0_r_kind))
 
 if (mype==0) then
-   write(6,'(A)')' ADTEST_BKGCOV             0.123456789012345678'
-   write(6,'(A,ES25.18)')' ADTEST_BKGCOV <B.Y,X>  = ',zz1
-   write(6,'(A,ES25.18)')' ADTEST_BKGCOV <Y,B.X>  = ',zz2
-   write(6,'(A,i3,   A)')' ADTEST_BGCOV ',idig,' digits are identical'
-   write(6,'(A,ES25.18)')' ADTEST_BKGCOV rel. err.= ',zz3
-   write(6,'(A,ES25.18)')' ADTEST_BKGCOV mach.eps = ',epsilon(zz3)
+   write(6,'(A)')' ADTEST_BKGERR             0.123456789012345678'
+   write(6,'(A,ES25.18)')' ADTEST_BKGERR <B.Y,X>  = ',zz1
+   write(6,'(A,ES25.18)')' ADTEST_BKGERR <Y,B.X>  = ',zz2
+   write(6,'(A,i3,   A)')' ADTEST_BKGERR ',idig,' digits are identical'
+   write(6,'(A,ES25.18)')' ADTEST_BKGERR rel. err.= ',zz3
+   write(6,'(A,ES25.18)')' ADTEST_BKGERR mach.eps = ',epsilon(zz3)
 endif
 
-iadtest_cnt(2) = iadtest_cnt(2) + 1
+call deallocate_cv (gz)
+call deallocate_cv (gy)
+call deallocate_cv (gx)
 
-end subroutine adtest_bkgcov
+iadtest_cnt(3) = iadtest_cnt(3) + 1
+
+end subroutine adtest_bkgerr_
 ! ----------------------------------------------------------------------
 
 subroutine adtest_stvp2uv(idx,mype)
@@ -365,7 +400,7 @@ subroutine adtest_stvp2uv(idx,mype)
   real(r_kind) zz1, zz2, zz3
   integer(i_kind) :: idig,i,j,k,idim
 
-  if (iadtest_cnt(3)>0) return
+  if (iadtest_cnt(4)>iadtest_maxiter) return
 
   idim=2
   allocate(x(idim,nlat,nlon))
@@ -399,7 +434,7 @@ subroutine adtest_stvp2uv(idx,mype)
   else
      zz3=abs(zz1-zz2)
   endif
-  idig= int(-log(zz3+tiny(zz3))/log(10.0_r_kind))
+  idig= int(-log(abs(zz3)+tiny(zz3))/log(10.0_r_kind))
 
   if (mype==0) then
     write(6,'(A)')' ADTEST_STVP2UV            0.123456789012345678'
@@ -410,7 +445,7 @@ subroutine adtest_stvp2uv(idx,mype)
     write(6,'(A,ES25.18)')' ADTEST_STVP2UV mach.eps = ',epsilon(zz3)
   endif
   deallocate(x,y,z)
-  iadtest_cnt(3) = iadtest_cnt(3) + 1
+  iadtest_cnt(4) = iadtest_cnt(4) + 1
 end subroutine adtest_stvp2uv
  
 real(r_kind) function my_dot_product(x,y)
